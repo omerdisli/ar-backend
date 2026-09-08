dbdf79dc680986c8a2db00d40030eb109ab79bf2
omerdisli <disliinsan3@gmail.com>
2026-09-08 22:02:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, File, UploadFile
 app = FastAPI()
 
 # API Key'inizi tırnak işaretlerini silmeden araya yazın
-KIRI_API_KEY = "https://ar-backend-aoqa.onrender.com/"
+KIRI_API_KEY = os.getenv("KIRI_API_KEY")
 @app.post("/upload-video")
 async def upload_video(file: UploadFile = File(...)):
     video_bytes = await file.read()
